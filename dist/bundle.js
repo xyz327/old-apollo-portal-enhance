5259ac089480d10b7db01b47bf143ad0beae8835
xyz327 <xyz327@outlook.com>
2022-11-20 21:58:22 +0800

    增加改动暂存功能

--- dist/bundle.js
+++ dist/bundle.js
@@ -52,18 +52,29 @@
       });
     });
     // 加载 layer  依赖 $
-    GM_addElement('script', {
-      src: 'https://cdn.jsdelivr.net/npm/layer-src@3.5.1/src/layer.js',
-      type: 'text/javascript'
+    GM_addElement("script", {
+      src: "https://cdn.jsdelivr.net/npm/layer-src@3.5.1/src/layer.js",
+      type: "text/javascript",
     });
   })();
+  function onNamesacpeLoaded(cb) {
+    $("body").on("namespaceLoaded", cb);
+  }
   loadFeature("onNamesacpeLoaded", true, function () {
     var $namespaces = $(".namespace-name");
     if ($namespaces.length == 0) {
       return false;
     }
-    console.log('trigger namespaceLoaded');
+    console.log("trigger namespaceLoaded");
     $("body").trigger("namespaceLoaded");
+    const observer = new MutationObserver(function () {
+      console.log("rebuild" );
+      $("body").trigger("namespaceLoaded");
+    });
+
+    $.each($(".config-item-container"), (index, el) => {
+      observer.observe(el, { childList: true });
+    });
   });
   function getAppId() {
     let hash = location.hash;
@@ -78,6 +89,7 @@
     loadFeature0(name, feature, false);
     if (reloadOnHashChange) {
       $(window).on("hashchange", function (e) {
+        console.log("hashchange");
         loadFeature0(name, feature, true);
       });
     }
@@ -574,5 +586,232 @@
         currItem = namesapce.items.find((e) => e.item.key === key);
       });
   });
+
+  loadFeature("stash", true, function () {
+    onNamesacpeLoaded(function () {
+      console.log("stash");
+
+      $.each(
+        $("section.master-panel-body.ng-scope>.panel-heading>"),
+        function (idx, el) {
+          var $panel = $(el);
+          if ($panel.find(".stashFeature").length > 0) {
+            return;
+          }
+          $panel.append(`<label class="stashFeature hidden"/>`);
+          var namespaceName = $panel
+            .find("b.namespace-name.ng-binding")
+            .text()
+            .trim();
+          var namespaceScope = $(
+            'div[ng-controller="ConfigNamespaceController"]'
+          ).scope();
+          var ConfigService = angular
+            .injector(["application"])
+            .get("ConfigService");
+          var namespace = namespaceScope.namespaces.find(
+            (e) => e.baseInfo.namespaceName === namespaceName
+          );
+          var $menu = $(el).find(".dropdown-menu");
+          $menu.append('<li role="separator" class="divider"></li>');
+          $(`
+            <li><a href="javascript:void(0);" ><span class="glyphicon glyphicon-save"></span>暂存改动</a></li>
+            `)
+            .on("click", function () {
+              layer.confirm("是否要暂存改动?", function (index) {
+                console.log(namespaceName);
+                console.log(namespace);
+                console.log(ConfigService);
+                var items = namespace.items.filter((item) => item.isModified);
+                if (items.length === 0) {
+                  layer.close(index);
+                  layer.msg("没有改动");
+                  return;
+                }
+                localStorage.setItem(
+                  getStashKey(namespaceScope, namespace),
+                  JSON.stringify(items)
+                );
+
+                // recovery old value
+                Promise.all(
+                  items.map((item) => {
+                    var isNewValue = item.isModified && !item.oldValue;
+                    var isDeleted = item.isDeleted;
+                    if (isNewValue) {
+                      // 新增的配置 删除
+                      return ConfigService.delete_item(
+                        namespace.baseInfo.appId,
+                        namespaceScope.pageContext.env,
+                        namespace.baseInfo.clusterName,
+                        namespace.baseInfo.namespaceName,
+                        item.item.id
+                      );
+                    }
+                    if (isDeleted) {
+                      // 被删除的数据 拿不到原来的 item 信息
+                      // 那就 create
+                      return ConfigService.create_item(
+                        namespace.baseInfo.appId,
+                        namespaceScope.pageContext.env,
+                        namespace.baseInfo.clusterName,
+                        namespace.baseInfo.namespaceName,
+                        {
+                          key: item.item.key,
+                          value: item.oldValue,
+                          tableViewOperType: "create",
+                          addItemBtnDisabled: true,
+                        }
+                      );
+                    }
+                    // 更新的 key
+                    return ConfigService.update_item(
+                      namespace.baseInfo.appId,
+                      namespaceScope.pageContext.env,
+                      namespace.baseInfo.clusterName,
+                      namespace.baseInfo.namespaceName,
+                      $.extend(item.item, {
+                        value: item.oldValue,
+                        tableViewOperType: "update",
+                      })
+                    );
+                  })
+                ).then(() => {
+                  layer.close(index);
+                  layer.msg("改动暂存成功!");
+                  // 直接 reload
+                  location.reload();
+                });
+              });
+            })
+            .appendTo($menu);
+
+          $(`
+            <li><a href="javascript:void(0);"><span class="glyphicon glyphicon-open"></span>恢复暂存改动</a></li>
+            `)
+            .on("click", function () {
+              var items = JSON.parse(
+                localStorage.getItem(getStashKey(namespaceScope, namespace))
+              );
+              if (!items || items.length === 0) {
+                layer.msg("没有暂存数据");
+                return;
+              }
+              layer.open({
+                content: getUnstashTable(items),
+                yes: function (index) {
+                  // recovery old value
+                  Promise.all(
+                    items.map((item) => {
+                      var isNewValue = item.isModified && !item.oldValue;
+                      var isDeleted = item.isDeleted;
+                      if (isDeleted) {
+                        // 重新拿一次 itemid
+                        item = namespace.items.filter(
+                          (v) => v.item.key === item.item.key
+                        )[0]; // 取第一个
+                        // 新增的配置 删除
+                        return ConfigService.delete_item(
+                          namespace.baseInfo.appId,
+                          namespaceScope.pageContext.env,
+                          namespace.baseInfo.clusterName,
+                          namespace.baseInfo.namespaceName,
+                          item.item.id
+                        );
+                      }
+                      if (isNewValue) {
+                        // 被删除的数据 拿不到原来的 item 信息
+                        // 那就 create
+                        return ConfigService.create_item(
+                          namespace.baseInfo.appId,
+                          namespaceScope.pageContext.env,
+                          namespace.baseInfo.clusterName,
+                          namespace.baseInfo.namespaceName,
+                          {
+                            key: item.item.key,
+                            value: item.newValue,
+                            tableViewOperType: "create",
+                            addItemBtnDisabled: true,
+                          }
+                        );
+                      }
+                      // 更新的 key
+                      return ConfigService.update_item(
+                        namespace.baseInfo.appId,
+                        namespaceScope.pageContext.env,
+                        namespace.baseInfo.clusterName,
+                        namespace.baseInfo.namespaceName,
+                        $.extend(item.item, {
+                          value: item.newValue,
+                          tableViewOperType: "update",
+                        })
+                      );
+                    })
+                  ).then(() => {
+                    layer.close(index);
+                    layer.msg("恢复暂存成功!");
+                    localStorage.removeItem(
+                      getStashKey(namespaceScope, namespace)
+                    );
+                    // 直接 reload
+                    location.reload();
+                  });
+                },
+              });
+            })
+            .appendTo($menu);
+          console.log("add stash", namespaceName);
+        }
+      );
+    });
+  });
+
+  function getStashKey(namespaceScope, namespace) {
+    var baseInfo = namespace.baseInfo;
+    return `stash_${namespaceScope.pageContext.env}_${baseInfo.appId}_${baseInfo.clusterName}_${baseInfo.namespaceName}`;
+  }
+  function getUnstashTable(changedItems) {
+    var tab = "";
+    for (var item of changedItems) {
+      tab += `<tr>
+          <td>${item.item.key}</td>
+          <td data-value="${item.newValue}">
+          ${
+            item.newValue.length < 150
+              ? item.newValue
+              : item.newValue.substring(0, 150) + "..."
+          }
+          </td>
+          <td>${item.item.comment ? item.item.comment : ""}</td>
+          <td>${
+            item.item.dataChangeLastModifiedTime
+              ? item.item.dataChangeLastModifiedTime
+              : ""
+          }</td>
+          </tr>`;
+    }
+    return `
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+      <tr>
+          <th>
+              Key
+          </th>
+          <th>
+              new Value
+          </th>
+          <th>
+              备注
+          </th>
+          <th>
+              最后修改时间
+          </th>
+          </tr>
+          </thead>
+          <tbody id="unstashTable">
+              ${tab}
+          </tbody>
+          </table>`;
+  }
 
 })();
--- src/stash.js
+++ src/stash.js
@@ -1,112 +1,227 @@
-// import { loadFeature, onNamesacpeLoaded } from "./base";
-// loadFeature("stash", true, function () {
-//   onNamesacpeLoaded(function () {
-//     console.log("stash");
+import { loadFeature, onNamesacpeLoaded } from "./base";
+loadFeature("stash", true, function () {
+  onNamesacpeLoaded(function () {
+    console.log("stash");
 
-//     $.each(
-//       $("section.master-panel-body.ng-scope>.panel-heading>"),
-//       function (idx, el) {
-//         console.log(el);
-//         var namespaceName = $(el)
-//           .find("b.namespace-name.ng-binding")
-//           .text()
-//           .trim();
-//         var namespaceScope = $(
-//           'div[ng-controller="ConfigNamespaceController"]'
-//         ).scope();
-//         var ConfigService = angular
-//           .injector(["application"])
-//           .get("ConfigService");
-//         var namespace = namespaceScope.namespaces.find(
-//           (e) => e.baseInfo.namespaceName === namespaceName
-//         );
-//         var modified = namespace.itemModifiedCnt > 0;
-//         if (modified) {
-//           $(`
-//             <button type="button" class="btn btn-warning btn-sm">
-//             <span class="glyphicon glyphicon-save"></span>
-//             暂存改动</button>
-//             `)
-//             .on("click", function () {
-//               console.log(namespaceName);
-//               console.log(namespace);
-//               stash(namespace);
-//             })
-//             .prependTo($(el).find(".header-buttons"));
-//           var $menu = $(el).find(".dropdown-menu");
-//           $menu.append('<li role="separator" class="divider"></li>');
-//           $(`
-//             <li><a href="javascript:void(0);" class=" btn-warning"><span class="glyphicon glyphicon-save"></span>暂存改动</a></li>
-//             `)
-//             .on("click", function () {
-//               layer.confirm("是否要暂存改动?", function (index) {
-//                 console.log(namespaceName);
-//                 console.log(namespace);
-//                 console.log(ConfigService);
-//                 var newVals = {};
-//                 var items = namespace.items.filter((item) => item.isModified);
-//                 items.forEach((item) => {
-//                   newVals[item.item.key] = {
-//                     value: item.newValue,
-//                     isDeleted: item.isDeleted,
-//                   };
-//                 });
-//                 localStorage.setItem(
-//                   `stash_${namespaceName}`,
-//                   JSON.stringify(newVals)
-//                 );
-//                 // recovery old value
-//                 Promise.all(
-//                   items.forEach((item) => {
-//                     return ConfigService.update_item(
-//                       namespace.baseInfo.appId,
-//                       namespaceScope.pageContext.env,
-//                       namespace.baseInfo.clusterName,
-//                       namespace.baseInfo.namespaceName,
-//                       item
-//                     );
-//                   })
-//                 ).then(() => {
-//                   layer.close(index);
-//                   layer.msg("改动暂存成功!");
-//                 });
-//               });
-//             })
-//             .appendTo($menu);
-//           $(`
-//             <li><a href="#" class=" btn-warning"><span class="glyphicon glyphicon-save"></span>恢复暂存改动</a></li>
-//             `).appendTo($menu);
-//         }
-//       }
-//     );
-//   });
-// });
+    $.each(
+      $("section.master-panel-body.ng-scope>.panel-heading>"),
+      function (idx, el) {
+        var $panel = $(el);
+        if ($panel.find(".stashFeature").length > 0) {
+          return;
+        }
+        $panel.append(`<label class="stashFeature hidden"/>`)
+        var namespaceName = $panel
+          .find("b.namespace-name.ng-binding")
+          .text()
+          .trim();
+        var namespaceScope = $(
+          'div[ng-controller="ConfigNamespaceController"]'
+        ).scope();
+        var ConfigService = angular
+          .injector(["application"])
+          .get("ConfigService");
+        var namespace = namespaceScope.namespaces.find(
+          (e) => e.baseInfo.namespaceName === namespaceName
+        );
+        var $menu = $(el).find(".dropdown-menu");
+        $menu.append('<li role="separator" class="divider"></li>');
+        $(`
+            <li><a href="javascript:void(0);" ><span class="glyphicon glyphicon-save"></span>暂存改动</a></li>
+            `)
+          .on("click", function () {
+            layer.confirm("是否要暂存改动?", function (index) {
+              console.log(namespaceName);
+              console.log(namespace);
+              console.log(ConfigService);
+              var items = namespace.items.filter((item) => item.isModified);
+              if (items.length === 0) {
+                layer.close(index);
+                layer.msg("没有改动");
+                return;
+              }
+              localStorage.setItem(
+                getStashKey(namespaceScope, namespace),
+                JSON.stringify(items)
+              );
 
-// $("body").append(`
-// <!-- Modal -->
-// <div class="modal fade" id="stashModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-//   <div class="modal-dialog modal-lg" role="document">
-//     <div class="modal-content">
-//       <div class="modal-header">
-//         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-//         <h4 class="modal-title"><span class="text-danger" id="diff-detail-title"></span> 差异对比</h4>
-//       </div>
-//       <div class="modal-body" >
-//           <div class="row">
-//             <div class="col-xs-6 text-center">
-//               <span data-tooltip="tooltip" title="点击复制" id="copyOld" data-copy="copy" data-copy-value=""  class="label label-default">旧值 
-//               <label class="glyphicon glyphicon-duplicate"></label>
-//               </span>
-//             </div>
-//             <div class="col-xs-6 text-center">
-//               <span data-tooltip="tooltip" title="点击复制" id="copyNew" data-copy="copy" data-copy-value="" class="label label-success">新值
-//               <label class="glyphicon glyphicon-duplicate"></label>
-//               </span>
-//             </div>
-//           </div>
-//           <div id="diff-container" style="display:flex"></div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// `);
+              // recovery old value
+              Promise.all(
+                items.map((item) => {
+                  var isNewValue = item.isModified && !item.oldValue;
+                  var isDeleted = item.isDeleted;
+                  if (isNewValue) {
+                    // 新增的配置 删除
+                    return ConfigService.delete_item(
+                      namespace.baseInfo.appId,
+                      namespaceScope.pageContext.env,
+                      namespace.baseInfo.clusterName,
+                      namespace.baseInfo.namespaceName,
+                      item.item.id
+                    );
+                  }
+                  if (isDeleted) {
+                    // 被删除的数据 拿不到原来的 item 信息
+                    // 那就 create
+                    return ConfigService.create_item(
+                      namespace.baseInfo.appId,
+                      namespaceScope.pageContext.env,
+                      namespace.baseInfo.clusterName,
+                      namespace.baseInfo.namespaceName,
+                      {
+                        key: item.item.key,
+                        value: item.oldValue,
+                        tableViewOperType: "create",
+                        addItemBtnDisabled: true,
+                      }
+                    );
+                  }
+                  // 更新的 key
+                  return ConfigService.update_item(
+                    namespace.baseInfo.appId,
+                    namespaceScope.pageContext.env,
+                    namespace.baseInfo.clusterName,
+                    namespace.baseInfo.namespaceName,
+                    $.extend(item.item, {
+                      value: item.oldValue,
+                      tableViewOperType: "update",
+                    })
+                  );
+                })
+              ).then(() => {
+                layer.close(index);
+                layer.msg("改动暂存成功!");
+                // 直接 reload
+                location.reload();
+              });
+            });
+          })
+          .appendTo($menu);
+
+        $(`
+            <li><a href="javascript:void(0);"><span class="glyphicon glyphicon-open"></span>恢复暂存改动</a></li>
+            `)
+          .on("click", function () {
+            var items = JSON.parse(
+              localStorage.getItem(getStashKey(namespaceScope, namespace))
+            );
+            if (!items || items.length === 0) {
+              layer.msg("没有暂存数据");
+              return;
+            }
+            layer.open({
+              content: getUnstashTable(items),
+              yes: function (index) {
+                // recovery old value
+                Promise.all(
+                  items.map((item) => {
+                    var isNewValue = item.isModified && !item.oldValue;
+                    var isDeleted = item.isDeleted;
+                    if (isDeleted) {
+                      // 重新拿一次 itemid
+                      item = namespace.items.filter(
+                        (v) => v.item.key === item.item.key
+                      )[0]; // 取第一个
+                      // 新增的配置 删除
+                      return ConfigService.delete_item(
+                        namespace.baseInfo.appId,
+                        namespaceScope.pageContext.env,
+                        namespace.baseInfo.clusterName,
+                        namespace.baseInfo.namespaceName,
+                        item.item.id
+                      );
+                    }
+                    if (isNewValue) {
+                      // 被删除的数据 拿不到原来的 item 信息
+                      // 那就 create
+                      return ConfigService.create_item(
+                        namespace.baseInfo.appId,
+                        namespaceScope.pageContext.env,
+                        namespace.baseInfo.clusterName,
+                        namespace.baseInfo.namespaceName,
+                        {
+                          key: item.item.key,
+                          value: item.newValue,
+                          tableViewOperType: "create",
+                          addItemBtnDisabled: true,
+                        }
+                      );
+                    }
+                    // 更新的 key
+                    return ConfigService.update_item(
+                      namespace.baseInfo.appId,
+                      namespaceScope.pageContext.env,
+                      namespace.baseInfo.clusterName,
+                      namespace.baseInfo.namespaceName,
+                      $.extend(item.item, {
+                        value: item.newValue,
+                        tableViewOperType: "update",
+                      })
+                    );
+                  })
+                ).then(() => {
+                  layer.close(index);
+                  layer.msg("恢复暂存成功!");
+                  localStorage.removeItem(
+                    getStashKey(namespaceScope, namespace)
+                  );
+                  // 直接 reload
+                  location.reload();
+                });
+              },
+            });
+          })
+          .appendTo($menu);
+        console.log("add stash", namespaceName);
+      }
+    );
+  });
+});
+
+function getStashKey(namespaceScope, namespace) {
+  var baseInfo = namespace.baseInfo;
+  return `stash_${namespaceScope.pageContext.env}_${baseInfo.appId}_${baseInfo.clusterName}_${baseInfo.namespaceName}`;
+}
+function getUnstashTable(changedItems) {
+  var tab = "";
+  for (var item of changedItems) {
+    tab += `<tr>
+          <td>${item.item.key}</td>
+          <td data-value="${item.newValue}">
+          ${
+            item.newValue.length < 150
+              ? item.newValue
+              : item.newValue.substring(0, 150) + "..."
+          }
+          </td>
+          <td>${item.item.comment ? item.item.comment : ""}</td>
+          <td>${
+            item.item.dataChangeLastModifiedTime
+              ? item.item.dataChangeLastModifiedTime
+              : ""
+          }</td>
+          </tr>`;
+  }
+  return `
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+      <tr>
+          <th>
+              Key
+          </th>
+          <th>
+              new Value
+          </th>
+          <th>
+              备注
+          </th>
+          <th>
+              最后修改时间
+          </th>
+          </tr>
+          </thead>
+          <tbody id="unstashTable">
+              ${tab}
+          </tbody>
+          </table>`;
+}
