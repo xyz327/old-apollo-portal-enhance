7800234aa9435929212b50e53855a86bca828064
席舟 <568353394@qq.com>
2025-08-08 10:37:16 +0800

    cdn

--- tampermonkey.header.js
+++ tampermonkey.header.js
@@ -10,10 +10,8 @@
 // @updateURL    https://raw.githubusercontent.com/xyz327/old-apollo-portal-enhance/main/{{destFile}}
 // @author       {{author}}
 // @match        *://*/config.html*
-// @resource     highlight_xcode_css https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/styles/xcode.min.css
-// @require      https://cdn.bootcdn.net/ajax/libs/diff_match_patch/20121119/diff_match_patch.js
-// @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/highlight.min.js
-// @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/languages/json.min.js
+// @resource     highlight_xcode_css https://cdn.jsdelivr.net/npm/highlight.js@9.18.5/styles/xcode.min.css
+// @require      https://cdn.jsdelivr.net/combine/npm/highlight.js@9.18.5,npm/highlight.js@9.18.5/lib/languages/json.min.js
 // @resource     text_different_css https://cdn.jsdelivr.net/npm/text-different@1.2.1/build/style/text-different.min.css
 // @require      https://cdn.jsdelivr.net/combine/npm/text-different@1.2.1/build/text-different.min.js,npm/text-different@1.2.1/build/text-different-for-html.min.js
 // @noframes
