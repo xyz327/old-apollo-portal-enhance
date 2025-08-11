d0adf9c9af9721c046ff936bea2d7952cd871e32
席舟 <568353394@qq.com>
2025-08-11 17:12:47 +0800

    Reapply "cdn"
    This reverts commit 6fb17c5a1a62decb906b80f3fff158b0865aa985.

--- tampermonkey-script.js
+++ tampermonkey-script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         apollo-enhance
 // @namespace    apollo-enhance
-// @version      0.9.16
+// @version      0.9.17
 // @description  make old apollo better
 // @homepage     https://github.com/xyz327/old-apollo-portal-enhance
 // @website      https://github.com/xyz327/old-apollo-portal-enhance
@@ -10,10 +10,8 @@
 // @updateURL    https://raw.githubusercontent.com/xyz327/old-apollo-portal-enhance/main/tampermonkey-script.js
 // @author       xizhou
 // @match        *://*/config.html*
-// @resource     highlight_xcode_css https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/styles/xcode.min.css
-// @require      https://cdn.bootcdn.net/ajax/libs/diff_match_patch/20121119/diff_match_patch.js
-// @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/highlight.min.js
-// @require      https://cdn.bootcdn.net/ajax/libs/highlight.js/9.18.5/languages/json.min.js
+// @resource     highlight_xcode_css https://cdn.jsdelivr.net/npm/highlight.js@9.18.5/styles/xcode.min.css
+// @require      https://cdn.jsdelivr.net/combine/npm/highlight.js@9.18.5,npm/highlight.js@9.18.5/lib/languages/json.min.js
 // @resource     text_different_css https://cdn.jsdelivr.net/npm/text-different@1.2.1/build/style/text-different.min.css
 // @require      https://cdn.jsdelivr.net/combine/npm/text-different@1.2.1/build/text-different.min.js,npm/text-different@1.2.1/build/text-different-for-html.min.js
 // @noframes
@@ -159,6 +157,7 @@
 
     loadCss("https://cdn.jsdelivr.net/npm/bootstrap-switch@3.3.4/dist/css/bootstrap3/bootstrap-switch.min.css");
     loadJs("https://cdn.jsdelivr.net/npm/bootstrap-switch@3.3.4/dist/js/bootstrap-switch.min.js");
+    loadJs("https://cdn.bootcdn.net/ajax/libs/diff_match_patch/20121119/diff_match_patch.js");
     const highlight_xcode_css = GM_getResourceText("highlight_xcode_css");
     const text_different_css = GM_getResourceText("text_different_css");
     GM_addStyle(highlight_xcode_css);
@@ -1423,7 +1422,7 @@
 
   });
 
-  BASE_INFO.version = "0.9.16";
+  BASE_INFO.version = "0.9.17";
   loadFeature("main", { switch: false }, function () {
     $("body").trigger("featureLoaded");
     console.log("trigger featureLoaded  v:", BASE_INFO);
